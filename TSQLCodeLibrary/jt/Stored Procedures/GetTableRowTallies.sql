b6e161c27602915833f08337ad8a8f1bb052f286
jamiet <jamie@jamiethomson.net>
2013-12-30 14:01:02 +0000

    GetTableRowTallies

---
+++ TSQLCodeLibrary/jt/Stored Procedures/GetTableRowTallies.sql
@@ -0,0 +1,17 @@
+﻿CREATE PROCEDURE jt.GetTableRowTallies
+	@dbName SYSNAME = 'master'
+AS
+BEGIN
+		DECLARE @sql NVARCHAR(MAX) = '
+		SELECT t.name
+			,s.partition_number
+			,s.row_count
+		FROM [@dbname].sys.dm_db_partition_stats AS s
+			INNER JOIN [@dbname].sys.tables AS t ON t.[object_id] = s.[object_id]
+		GROUP BY t.name
+			,s.partition_number
+			,s.row_count;
+		';
+		SET	@sql = REPLACE(@sql,'@dbName',@dbName)
+		EXEC (@sql)
+END
