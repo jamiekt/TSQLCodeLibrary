6ad5f27bb8a1630c6c8335d95a34414b82a516ff
jamiet <jamie@jamiethomson.net>
2013-12-30 13:51:37 +0000

    vwDiskFileLatency

--- TSQLCodeLibrary/Scripts/ScriptsIgnoredOnImport.sql
+++ TSQLCodeLibrary/Scripts/ScriptsIgnoredOnImport.sql
@@ -1,19 +1,83 @@
 ﻿
-USE [CUECommonReference]
+create view jt.vwdiskfileLatency
+AS
+/*
+	Show disk file latency
+	
+	http://www.sqlskills.com/BLOGS/PAUL/category/IO-Subsystems.aspx
+*/
+
+
+SELECT 
+    --virtual file latency
+    ReadLatency = CASE WHEN num_of_reads = 0
+        THEN 0 ELSE (io_stall_read_ms / num_of_reads) END,
+    WriteLatency = CASE WHEN num_of_writes = 0 
+        THEN 0 ELSE (io_stall_write_ms / num_of_writes) END,
+    Latency = CASE WHEN (num_of_reads = 0 AND num_of_writes = 0)
+        THEN 0 ELSE (io_stall / (num_of_reads + num_of_writes)) END,
+    --avg bytes per IOP
+    AvgBPerRead = CASE WHEN num_of_reads = 0 
+        THEN 0 ELSE (num_of_bytes_read / num_of_reads) END,
+    AvgBPerWrite = CASE WHEN io_stall_write_ms = 0 
+        THEN 0 ELSE (num_of_bytes_written / num_of_writes) END,
+    AvgBPerTransfer = CASE WHEN (num_of_reads = 0 AND num_of_writes = 0)
+        THEN 0 ELSE ((num_of_bytes_read + num_of_bytes_written) / 
+            (num_of_reads + num_of_writes)) END,    
+    LEFT (mf.physical_name, 2) AS Drive,
+    DB_NAME (vfs.database_id) AS DB,
+    --vfs.*,
+    mf.physical_name
+FROM sys.dm_io_virtual_file_stats (NULL,NULL) AS vfs
+JOIN sys.master_files AS mf
+    ON vfs.database_id = mf.database_id
+    AND vfs.file_id = mf.file_id
+--WHERE vfs.file_id = 2 -- log files
+-- ORDER BY Latency DESC
+-- ORDER BY ReadLatency DESC
+ORDER BY WriteLatency DESC;
+
 GO
 
-SET ANSI_NULLS ON
-GO
+--Syntax Error: The ORDER BY clause is not valid in views, inline functions, derived tables, sub-queries, and common table expressions, unless TOP or FOR XML or OFFSET is also specified.
+--create view jt.vwdiskfileLatency
+--AS
+--/*
+--	Show disk file latency
+--	
+--	http://www.sqlskills.com/BLOGS/PAUL/category/IO-Subsystems.aspx
+--*/
+--
+--
+--SELECT 
+--    --virtual file latency
+--    ReadLatency = CASE WHEN num_of_reads = 0
+--        THEN 0 ELSE (io_stall_read_ms / num_of_reads) END,
+--    WriteLatency = CASE WHEN num_of_writes = 0 
+--        THEN 0 ELSE (io_stall_write_ms / num_of_writes) END,
+--    Latency = CASE WHEN (num_of_reads = 0 AND num_of_writes = 0)
+--        THEN 0 ELSE (io_stall / (num_of_reads + num_of_writes)) END,
+--    --avg bytes per IOP
+--    AvgBPerRead = CASE WHEN num_of_reads = 0 
+--        THEN 0 ELSE (num_of_bytes_read / num_of_reads) END,
+--    AvgBPerWrite = CASE WHEN io_stall_write_ms = 0 
+--        THEN 0 ELSE (num_of_bytes_written / num_of_writes) END,
+--    AvgBPerTransfer = CASE WHEN (num_of_reads = 0 AND num_of_writes = 0)
+--        THEN 0 ELSE ((num_of_bytes_read + num_of_bytes_written) / 
+--            (num_of_reads + num_of_writes)) END,    
+--    LEFT (mf.physical_name, 2) AS Drive,
+--    DB_NAME (vfs.database_id) AS DB,
+--    --vfs.*,
+--    mf.physical_name
+--FROM sys.dm_io_virtual_file_stats (NULL,NULL) AS vfs
+--JOIN sys.master_files AS mf
+--    ON vfs.database_id = mf.database_id
+--    AND vfs.file_id = mf.file_id
+----WHERE vfs.file_id = 2 -- log files
+---- ORDER BY Latency DESC
+---- ORDER BY ReadLatency DESC
+--ORDER BY WriteLatency DESC;
 
-SET QUOTED_IDENTIFIER ON
-GO
 
-IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[csp_ToolIDAssign]') AND type in (N'P', N'PC'))
-DROP PROCEDURE [dbo].[csp_ToolIDAssign]
-GO
 
-SET ANSI_NULLS ON
-GO
-
-SET QUOTED_IDENTIFIER ON
 GO
